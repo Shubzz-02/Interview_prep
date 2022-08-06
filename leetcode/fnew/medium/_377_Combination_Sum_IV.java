d6d1e1c590e489bd251c4d180aeb976290ba0955
Shubzz <19619948+Shubzz-02@users.noreply.github.com>
2022-08-06 20:21:50 +0530

    updated _858_Mirror_Reflection.java

---
+++ leetcode/fnew/medium/_377_Combination_Sum_IV.java
@@ -0,0 +1,25 @@
+package com.company.leetcode.fnew.medium;
+
+public class _377_Combination_Sum_IV {
+
+    public static void main(String[] args) {
+
+    }
+
+    int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+
+        dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int n : nums) {
+                if (i >= n) {
+                    dp[i] += dp[i - n];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+}
