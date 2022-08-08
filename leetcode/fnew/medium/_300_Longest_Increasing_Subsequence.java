7768b624b2d283482fb8385b33e8b6a58c6dcc27
Shubzz <19619948+Shubzz-02@users.noreply.github.com>
2022-08-08 23:41:32 +0530

    updated _300_Longest_Increasing_Subsequence.java

--- leetcode/fnew/medium/_300_Longest_Increasing_Subsequence.java
+++ leetcode/fnew/medium/_300_Longest_Increasing_Subsequence.java
@@ -1,6 +1,7 @@
 package com.company.leetcode.fnew.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _300_Longest_Increasing_Subsequence {
@@ -15,12 +16,28 @@ public class _300_Longest_Increasing_Subsequence {
 
         int[] arr = {0, 1, 0, 3, 2, 3};
 
-        System.out.println(new _300_Longest_Increasing_Subsequence().lengthOfLTSRec(arr));
+        System.out.println(new _300_Longest_Increasing_Subsequence().lengthOfLIS(arr));
 
     }
 
     public int lengthOfLIS(int[] nums) {
-        return 0;
+        int[] dp = new int[nums.length];
+
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
 
     }
 
