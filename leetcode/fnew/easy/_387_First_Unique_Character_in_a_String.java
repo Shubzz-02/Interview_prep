47dd637b5b8d1d5b6f532fb72bfc8cd43a649557
Shubzz <19619948+Shubzz-02@users.noreply.github.com>
2022-08-16 13:17:46 +0530

    added _387_First_Unique_Character_in_a_String.java

---
+++ leetcode/fnew/easy/_387_First_Unique_Character_in_a_String.java
@@ -0,0 +1,30 @@
+package com.company.leetcode.fnew.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _387_First_Unique_Character_in_a_String {
+
+    public static void main(String[] args) {
+
+        String s = "leetcode";
+        System.out.println(new _387_First_Unique_Character_in_a_String().firstUniqChar(s));
+
+    }
+
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
