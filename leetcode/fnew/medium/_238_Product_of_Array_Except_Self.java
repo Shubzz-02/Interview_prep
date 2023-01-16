b8120c740e7e7b648a200693bf5b47b909da67e7
Shubzz <19619948+Shubzz-02@users.noreply.github.com>
2023-01-16 18:54:06 +0530

    added  _238_Product_of_Array_Except_Self

---
+++ leetcode/fnew/medium/_238_Product_of_Array_Except_Self.java
@@ -0,0 +1,40 @@
+package com.company.leetcode.fnew.medium;
+
+import java.util.Arrays;
+
+public class _238_Product_of_Array_Except_Self {
+
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4};
+
+        System.out.println(
+                Arrays.toString(new _238_Product_of_Array_Except_Self().productExceptSelf(num)));
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] prefix = new int[nums.length];
+        int[] postfix = new int[nums.length];
+
+        prefix[0] = nums[0];
+        postfix[nums.length - 1] = nums[nums.length - 1];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] * nums[i];
+            postfix[(nums.length - 1) - i] =
+                    postfix[(nums.length - 1) - i + 1] * nums[(nums.length - 1) - i];
+        }
+
+        int[] ans = new int[nums.length];
+
+        ans[0] = postfix[1];
+        ans[nums.length - 1] = prefix[nums.length - 2];
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            ans[i] = prefix[i - 1] * postfix[i + 1];
+        }
+
+        return ans;
+    }
+
+}
