1e67e0ae8436f35011d5341cad62d8ba42422123
Shubzz <19619948+Shubzz-02@users.noreply.github.com>
2023-01-16 18:23:56 +0530

    added _347_Top_K_Frequent_Elements

--- leetcode/fnew/medium/_347_Top_K_Frequent_Elements.java
+++ leetcode/fnew/medium/_347_Top_K_Frequent_Elements.java
@@ -1,18 +1,53 @@
 package com.company.leetcode.fnew.medium;
 
 
-
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
 
 public class _347_Top_K_Frequent_Elements {
 
     public static void main(String[] args) {
-        
+
+        int[] nums = new int[]{1, 1, 1, 2, 2, 3};
+        int k = 2;
+
+        System.out.println(
+                Arrays.toString(new _347_Top_K_Frequent_Elements().topKFrequent(nums, k)));
 
     }
 
     public int[] topKFrequent(int[] nums, int k) {
 
-        return new int[1];
+        int[] ans = new int[k];
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+
+        PriorityQueue<Map.Entry<Integer,Integer>> pq= new PriorityQueue<>(
+                Comparator.comparingInt(Entry::getValue));
+
+        for (Map.Entry<Integer,Integer> i : map.entrySet()){
+            pq.add(i);
+            if(pq.size() > k)
+                pq.poll();
+        }
+
+
+        int i = k;
+
+        while (!pq.isEmpty()){
+            ans[--i] = pq.poll().getKey();
+        }
+
+        return ans;
 
     }
 
