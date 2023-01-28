13094dc73293f860ef36b31ce4ad680315f4b9c1
Shubzz <19619948+Shubzz-02@users.noreply.github.com>
2023-01-28 22:28:46 +0530

    added _33_Search_in_Rotated_Sorted_Array

---
+++ leetcode/fnew/medium/_33_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,14 @@
+package com.company.leetcode.fnew.medium;
+
+public class _33_Search_in_Rotated_Sorted_Array {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int search(int[] nums, int target) {
+
+       return 0;
+    }
+
+}
